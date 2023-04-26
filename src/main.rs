cf13d9987cd23e5c6485ae749d52123e7ca69c57
piojagcodes <piojag@gmail.com>
2023-04-26 16:05:51 +0000

    Implementation of handler called hello. Small refactor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 
 use axum::response::Html;
+use axum::response::IntoResponse;
 use std::net::SocketAddr;
 use axum::Router;
 use axum::routing::get;
@@ -9,16 +10,26 @@ use axum::routing::get;
 async fn main() {
   let routes_hello = Router::new().route(
         "/hello",
-        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+        get(handler_hello),
     );  
 
+
+// region: --- Start Server
   let addr = SocketAddr::from(([127,0,0,1], 8080));
   println!("->> LISTENING on {addr}\n");
   axum::Server::bind(&addr)
       .serve(routes_hello.into_make_service())
       .await
       .unwrap();
-    
+
+  // endregion: --- Start Server
+}
+
+//  region: --- Handler Hello
+async fn handler_hello() -> impl IntoResponse {
+    println!("->> {:12} - handler_hello", "HANDLER");
+
+// endregion: --- Handler Hello
 }
 
 
